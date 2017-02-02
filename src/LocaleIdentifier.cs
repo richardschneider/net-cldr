37fa21aecf38685b561fbad252b160751a3be2a2
Richard Schneider <makaretu@gmail.com>
2017-02-02 14:37:36 +1300

    feat(LocaleIdentifier): parsing

---
+++ src/LocaleIdentifier.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Makaretu.Globalization
+{
+    /// <summary>
+    ///   Identifies a set of preferences that tend to be shared across significant swaths of the world.
+    /// </summary>
+    /// <remarks>
+    ///   An identifier is based on <see href="https://www.rfc-editor.org/rfc/bcp/bcp47.txt">BCP47</see>
+    ///   and <see href="http://www.unicode.org/reports/tr35/#Unicode_locale_identifier">Unicode TR 35</see> 
+    ///   for distinguishing among languages, locales, regions, currencies, 
+    ///   time zones, transforms, and so on. 
+    /// </remarks>
+    public class LocaleIdentifier
+    {
+        const string langP = @"(?<lang>[a-zA-z]{2,3}|[a-zA-Z]{5,8})";
+        const string scriptP = @"(?<script>[a-zA-z]{4})";
+        const string regionP = @"(?<region>[a-zA-z]{2}|\d{3})";
+        const string sepP = @"[-_]";
+        static string pattern =
+            "^(root" +
+                $"|{langP}({sepP}{scriptP})?" +
+                $"|{scriptP})" +
+            $"({sepP}{regionP})?" +
+            "$"
+            ;
+        static Regex idRegex = new Regex(pattern, RegexOptions.Compiled);
+
+        /// <summary>
+        ///   The language subtag.
+        /// </summary>
+        /// <value>
+        ///   ISO 639 code.
+        /// </value>
+        /// <remarks>
+        ///   All subtags are case insensitive but stored in the lower-case form.
+        /// </remarks>
+        public string Language { get; private set; }
+
+        /// <summary>
+        ///   The script (writing system) subtag.
+        /// </summary>
+        /// <value>
+        ///   ISO 15924 code.
+        /// </value>
+        /// <remarks>
+        ///   All subtags are case insensitive but stored in the lower-case form.
+        /// </remarks>
+        public string Script { get; private set; }
+
+        /// <summary>
+        ///   The region subtag.
+        /// </summary>
+        /// <value>
+        ///   ISO 3166-1 or UN M.49 code.
+        /// </value>
+        /// <remarks>
+        ///   All subtags are case insensitive but stored in the lower-case form.
+        /// </remarks>
+        public string Region { get; private set; }
+
+        /// <summary>
+        ///   Parses the string representation of a locale identifier to a LanguageIdentifier.
+        /// </summary>
+        /// <param name="s">
+        ///   A case insensitive string containing a locale identifier, based on BCP47.
+        /// </param>
+        /// <exception cref="FormatException">
+        ///   <paramref name="s"/> is not in the correct format.
+        /// </exception>
+        /// <remarks>
+        ///   A local identifier that refers to <paramref name="s"/>.
+        /// </remarks>
+        public static LocaleIdentifier Parse(string s)
+        {
+            LocaleIdentifier id;
+            if (TryParse(s, out id))
+                return id;
+
+            throw new FormatException($"'{s}' is not a valid locale identifier.");
+        }
+
+        /// <summary>
+        ///   Tries parsing the string representation of a locale identifier.
+        /// </summary>
+        /// <param name="s">
+        ///   A case insensitive string containing a locale identifier, based on BCP47.
+        /// </param>
+        /// <param name="result">
+        ///   A local identifier that refers to <paramref name="s"/> or <b>null</b> if the parsing
+        ///   failed.
+        /// </param>
+        /// <returns>
+        ///   <b>true</b> if <paramref name="s"/> was parsed successfully; otherwise, <b>false</b>.
+        /// </returns>
+        /// <remarks>
+        ///   A local identifier that refers to <paramref name="s"/>.
+        /// </remarks>
+        public static bool TryParse(string s, out LocaleIdentifier result)
+        {
+            var match = idRegex.Match(s.ToLowerInvariant());
+            if (!match.Success)
+            {
+                result = null;
+                return false;
+            }
+
+            result = new LocaleIdentifier
+            {
+                Language = match.Groups["lang"].Value,
+                Script = match.Groups["script"].Value,
+                Region = match.Groups["region"].Value
+            };
+            return true;
+        }
+    }
+}
---
+++ test/LocaleIdentifierTest.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Makaretu.Globalization
+{
+
+    [TestClass]
+    public class LocaleIdentifierTest
+    {
+        [TestMethod]
+        public void Parsing_Language()
+        {
+            var id = LocaleIdentifier.Parse("en");
+            Assert.AreEqual("en", id.Language);
+
+            Assert.IsFalse(LocaleIdentifier.TryParse("NotALanguageCode", out id));
+        }
+
+        [TestMethod]
+        public void Parsing_Language_Country()
+        {
+            var id = LocaleIdentifier.Parse("en-US");
+            Assert.AreEqual("en", id.Language);
+            Assert.AreEqual("us", id.Region);
+
+            Assert.IsFalse(LocaleIdentifier.TryParse("en-NotACountryCode", out id));
+        }
+
+        [TestMethod]
+        public void Parsing_Language_Region()
+        {
+            var id = LocaleIdentifier.Parse("es-419");
+            Assert.AreEqual("es", id.Language);
+            Assert.AreEqual("419", id.Region);
+
+            Assert.IsFalse(LocaleIdentifier.TryParse("es-1234", out id));
+        }
+
+        [TestMethod]
+        public void Parsing_Language_Script()
+        {
+            var  id = LocaleIdentifier.Parse("uz-Cyrl");
+            Assert.AreEqual("uz", id.Language);
+            Assert.AreEqual("cyrl", id.Script);
+
+            Assert.IsFalse(LocaleIdentifier.TryParse("es-NotScript", out id));
+        }
+
+        [TestMethod]
+        public void Parsing_Language_Script_Country()
+        {
+            var id = LocaleIdentifier.Parse("zh-Hant-CN");
+            Assert.AreEqual("zh", id.Language);
+            Assert.AreEqual("hant", id.Script);
+            Assert.AreEqual("cn", id.Region);
+
+            Assert.IsFalse(LocaleIdentifier.TryParse("zh-Hant-NotACountry", out id));
+        }
+
+        [TestMethod]
+        public void Parsing_Script()
+        {
+            var id = LocaleIdentifier.Parse("Cyrl");
+            Assert.AreEqual("cyrl", id.Script);
+
+            Assert.IsFalse(LocaleIdentifier.TryParse("NotAScript", out id));
+        }
+
+        [TestMethod]
+        public void Parsing_Script_Region()
+        {
+            var id = LocaleIdentifier.Parse("Cyrl-RU");
+            Assert.AreEqual("cyrl", id.Script);
+            Assert.AreEqual("ru", id.Region);
+
+            Assert.IsFalse(LocaleIdentifier.TryParse("Cyrl-NotACounty", out id));
+        }
+
+        [TestMethod]
+        public void Parsing_Underscore_Separator()
+        {
+            var id = LocaleIdentifier.Parse("zh_Hant_CN");
+            Assert.AreEqual("zh", id.Language);
+            Assert.AreEqual("hant", id.Script);
+            Assert.AreEqual("cn", id.Region);
+        }
+
+        [TestMethod]
+        public void Parsing_Throws()
+        {
+            ExceptionAssert.Throws<FormatException>(() => LocaleIdentifier.Parse("ThisIsNotALanguage"));
+        }
+
+    }
+}
