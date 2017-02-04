858589748394d5269911f46c15e85e9c0156d7f4
Richard Schneider <makaretu@gmail.com>
2017-02-04 22:59:53 +1300

    feat(StringRange): Enumerate the strings specified by a Unicode String Range
    This only implements a special case where Y has length 1.  This is good enough to process the current (30.0.3) XML files.

---
+++ src/StringRange.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makaretu.Globalization
+{
+    /// <summary>
+    ///   A String Range is a compact format for specifying a list of strings.
+    /// </summary>
+    /// <see href="http://www.unicode.org/reports/tr35/tr35.html#String_Range">TR #35</see> 
+    public static class StringRange
+    {
+        /// <summary>
+        ///   List the strings between X and Y.
+        /// </summary>
+        /// <param name="x">The starting string, inclusive.</param>
+        /// <param name="y">The ending string, inclusive.</param>
+        /// <returns>
+        ///   An inclusive sequence of strings between <paramref name="x"/>
+        ///   and <paramref name="y"/>.
+        /// </returns>
+        public static IEnumerable<string> Enumerate(string x, string y)
+        {
+            if (x.Length < 1)
+                throw new ArgumentOutOfRangeException(nameof(x), "Must have a character.");
+            if (y.Length > x.Length)
+                throw new ArgumentOutOfRangeException(nameof(y), "Too many characters.");
+
+            // TODO
+            if (y.Length > 1)
+                throw new ArgumentOutOfRangeException(nameof(y), "NYI too many characters.");
+
+            var n = y.Length;
+            var p = x.Substring(0, x.Length - n);
+            var s = x.Substring(x.Length - n, n);
+
+            for (int i = 0; i < n; ++i)
+            {
+                var from = s[i];
+                var to = y[i];
+                for (char code = from; code <= to; ++code)
+                {
+                    yield return p + code;
+                }
+            }
+        }
+
+    }
+}
---
+++ test/StringRangeTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Makaretu.Globalization
+{
+
+    [TestClass]
+    public class StringRangeTest
+    {
+        [TestMethod, Ignore]
+        public void TR35_Example_1()
+        {
+            var strings = StringRange.Enumerate("ab", "ad").ToArray();
+            Assert.AreEqual(3, strings.Length);
+            CollectionAssert.Contains(strings, "ab");
+            CollectionAssert.Contains(strings, "ac");
+            CollectionAssert.Contains(strings, "ad");
+        }
+
+        [TestMethod]
+        public void TR35_Example_2()
+        {
+            var strings = StringRange.Enumerate("ab", "d").ToArray();
+            Assert.AreEqual(3, strings.Length);
+            CollectionAssert.Contains(strings, "ab");
+            CollectionAssert.Contains(strings, "ac");
+            CollectionAssert.Contains(strings, "ad");
+        }
+
+        [TestMethod, Ignore]
+        public void TR35_Example_3()
+        {
+            var strings = StringRange.Enumerate("ab", "cd").ToArray();
+            Assert.AreEqual(9, strings.Length);
+            CollectionAssert.Contains(strings, "ab");
+            CollectionAssert.Contains(strings, "ac");
+            CollectionAssert.Contains(strings, "ad");
+            CollectionAssert.Contains(strings, "bb");
+            CollectionAssert.Contains(strings, "bc");
+            CollectionAssert.Contains(strings, "bd");
+            CollectionAssert.Contains(strings, "cb");
+            CollectionAssert.Contains(strings, "cc");
+            CollectionAssert.Contains(strings, "cd");
+        }
+    }
+}
