f270fd27be196cdac4c7c6b24a2040629a72fec3
Richard Schneider <makaretu@gmail.com>
2017-02-07 14:24:20 +1300

    feat(Locale): Add Locale class

---
+++ src/Locale.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makaretu.Globalization
+{
+    /// <summary>
+    ///   A set of preferences that tend to be shared across significant swaths of the world.
+    /// </summary>
+    public class Locale
+    {
+        /// <summary>
+        ///   Creates a new instance of the <see cref="Locale"/> class with the
+        ///   specified <see cref="LocaleIdentifier"/>.
+        /// </summary>
+        /// <param name="id">
+        ///   Identifies a set of preferences.
+        /// </param>
+        /// <remarks>
+        ///   The Create method should be used, so that locale caching can be used.
+        /// </remarks>
+        public Locale(LocaleIdentifier id)
+        {
+            Id = id;
+        }
+
+        /// <summary>
+        ///   Identifies a set of preferences for the locale.
+        /// </summary>
+        public LocaleIdentifier Id { get; private set; }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return Id.ToString();
+        }
+
+        /// <summary>
+        ///   Creates or reuses a locale with the specified string locale identifier.
+        /// </summary>
+        /// <param name="id">
+        ///   A case insensitive string containing a locale identifier, based on BCP47.
+        /// </param>
+        /// <exception cref="FormatException">
+        ///   <paramref name="id"/> is not in the correct format.
+        /// </exception>
+        /// <returns>
+        ///   A locale for the specified <paramref name="id"/>.
+        /// </returns>
+        /// <seealso cref="LocaleIdentifier.Parse"/>
+        public static Locale Create(string id)
+        {
+            return Create(LocaleIdentifier.Parse(id));
+        }
+
+        /// <summary>
+        ///   Creates or reuses a locale with the specified <see cref="LocaleIdentifier"/>.
+        /// </summary>
+        /// <param name="id">
+        ///   A locale identifier.
+        /// </param>
+        /// <returns>
+        ///   A locale for the specified <paramref name="id"/>.
+        /// </returns>
+        public static Locale Create(LocaleIdentifier id)
+        {
+            // TODO: Caching
+
+            return new Locale(id);
+        }
+
+    }
+}
---
+++ test/LocaleTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Makaretu.Globalization
+{
+
+    [TestClass]
+    public class LocaleTest
+    {
+        [TestMethod]
+        public void Creation()
+        {
+            var locale = Locale.Create("en");
+            Assert.AreEqual("en", locale.Id.ToString());
+        }
+
+        [TestMethod]
+        public void Stringify()
+        {
+            var locale = Locale.Create("zh-u-nu-finance");
+            Assert.AreEqual(locale.Id.ToString(), locale.ToString());
+        }
+
+    }
+}
