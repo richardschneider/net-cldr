a4d78f01235e26794c25e546f4289611af5e9ba5
Richard Schneider <makaretu@gmail.com>
2017-02-11 15:47:04 +1300

    feat(NumberSymbols): localised symbols for numbers

---
+++ src/Numbers/NumberSymbols.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makaretu.Globalization.Numbers
+{
+    /// <summary>
+    ///   The localised symbols that are commonly used when formatting numbers in a given locale. 
+    /// </summary>
+    public class NumberSymbols
+    {
+        string currencyDecimal;
+        string currencyGroup;
+
+        /// <summary>
+        ///   Separates the integer and fractional part of the number.
+        /// </summary>
+        public string Decimal { get; set; }
+
+        /// <summary>
+        ///   Separates clusters of integer digits to make large numbers more legible.
+        /// </summary>
+        public string Group { get; set; }
+
+        /// <summary>
+        ///   Symbol used to separate numbers in a list intended to represent structured data such as an array.
+        /// </summary>
+        public string List { get; set; }
+
+        /// <summary>
+        ///   Symbol used to indicate a percentage (1/100th) amount.
+        /// </summary>
+        public string PercentSign { get; set; }
+
+        /// <summary>
+        ///   Symbol used to denote negative value.
+        /// </summary>
+        public string MinusSign { get; set; }
+
+        /// <summary>
+        ///   Symbol used to denote positive value.
+        /// </summary>
+        public string PlusSign { get; set; }
+
+        /// <summary>
+        ///   Symbol separating the mantissa and exponent values.
+        /// </summary>
+        public string Exponential { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string SuperscriptingExponent { get; set; }
+
+        /// <summary>
+        ///   Symbol used to indicate a per-mille (1/1000th) amount.
+        /// </summary>
+        public string PerMille { get; set; }
+
+        /// <summary>
+        ///   The infinity sign.
+        /// </summary>
+        public string Infinity { get; set; }
+
+        /// <summary>
+        ///   The NaN sign.
+        /// </summary>
+        public string NotANumber { get; set; }
+
+        /// <summary>
+        ///   Separates the integer and fractional part of the currency number.
+        /// </summary>
+        /// <value>Defaults to <see cref="Decimal"/>.</value>
+        public string CurrencyDecimal
+        {
+            get { return currencyDecimal ?? Decimal; }
+            set { currencyDecimal = value; }
+        }
+
+        /// <summary>
+        ///   Separates clusters of integer digits to make large numbers more legible.
+        /// </summary>
+        /// <value>Defaults to <see cref="Group"/>.</value>
+        public string CurrencyGroup
+        {
+            get { return currencyGroup ?? Group; }
+            set { currencyGroup = value; }
+        }
+
+        /// <summary>
+        ///   Creates the symbols for the specified <see cref="Locale"/>.
+        /// </summary>
+        /// <param name="locale">
+        ///   The locale.
+        /// </param>
+        /// <returns>
+        ///   The symbols that are the best for the <paramref name="locale"/>.
+        /// </returns>
+        public static NumberSymbols Create(Locale locale)
+        {
+            var ns = NumberingSystem.Create(locale).Id;
+            var path = $"ldml/numbers/symbols[@numberSystem='{ns}']/";
+            var symbols = new NumberSymbols
+            {
+                Decimal = locale.Find(path + "decimal").Value,
+                Exponential = locale.Find(path + "exponential").Value,
+                Group = locale.Find(path + "group").Value,
+                Infinity = locale.Find(path + "infinity").Value,
+                List = locale.Find(path + "list").Value,
+                MinusSign = locale.Find(path + "minusSign").Value,
+                NotANumber = locale.Find(path + "nan").Value,
+                PercentSign = locale.Find(path + "percentSign").Value,
+                PerMille = locale.Find(path + "perMille").Value,
+                PlusSign = locale.Find(path + "plusSign").Value,
+                SuperscriptingExponent = locale.Find(path + "superscriptingExponent").Value
+            };
+
+            var found = locale.FindOrDefault(path + "currencyDecimal");
+            if (found != null)
+                symbols.CurrencyDecimal = found.Value;
+
+            found = locale.FindOrDefault(path + "currencyGroup");
+            if (found != null)
+                symbols.CurrencyGroup = found.Value;
+
+            return symbols;
+        }
+    }
+}
---
+++ test/Numbers/NumberSymbolsTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Makaretu.Globalization.Numbers
+{
+
+    [TestClass]
+    public class NumberSymbolsTest
+    {
+        [TestMethod]
+        public void Currency_Defaults()
+        {
+            var symbols = new NumberSymbols
+            {
+                Decimal = ".",
+                Group = ","
+            };
+            Assert.AreEqual(".", symbols.CurrencyDecimal);
+            Assert.AreEqual(",", symbols.CurrencyGroup);
+
+            symbols = new NumberSymbols
+            {
+                CurrencyDecimal = ".X",
+                CurrencyGroup = ",X"
+            };
+            Assert.AreEqual(".X", symbols.CurrencyDecimal);
+            Assert.AreEqual(",X", symbols.CurrencyGroup);
+        }
+
+        [TestMethod]
+        public void Create_From_Locale()
+        {
+            var locale = Locale.Create("de");
+            var symbols = NumberSymbols.Create(locale);
+            Assert.AreEqual(",", symbols.CurrencyDecimal);
+            Assert.AreEqual(".", symbols.CurrencyGroup);
+            Assert.AreEqual(",", symbols.Decimal);
+            Assert.AreEqual("E", symbols.Exponential);
+            Assert.AreEqual(".", symbols.Group);
+            Assert.AreEqual("∞", symbols.Infinity);
+            Assert.AreEqual(";", symbols.List);
+            Assert.AreEqual("-", symbols.MinusSign);
+            Assert.AreEqual("NaN", symbols.NotANumber);
+            Assert.AreEqual("%", symbols.PercentSign);
+            Assert.AreEqual("‰", symbols.PerMille);
+            Assert.AreEqual("+", symbols.PlusSign);
+            Assert.AreEqual("·", symbols.SuperscriptingExponent);
+
+            locale = Locale.Create("de-AT");
+            symbols = NumberSymbols.Create(locale);
+            Assert.AreEqual(",", symbols.CurrencyDecimal);
+            Assert.AreEqual(".", symbols.CurrencyGroup);
+            Assert.AreEqual(",", symbols.Decimal);
+            Assert.AreEqual("E", symbols.Exponential);
+            Assert.AreEqual("\u00A0", symbols.Group);
+            Assert.AreEqual("∞", symbols.Infinity);
+            Assert.AreEqual(";", symbols.List);
+            Assert.AreEqual("-", symbols.MinusSign);
+            Assert.AreEqual("NaN", symbols.NotANumber);
+            Assert.AreEqual("%", symbols.PercentSign);
+            Assert.AreEqual("‰", symbols.PerMille);
+            Assert.AreEqual("+", symbols.PlusSign);
+            Assert.AreEqual("·", symbols.SuperscriptingExponent);
+        }
+    }
+}
