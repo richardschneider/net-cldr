44d5ffdd553532c8946841f1f06d4aaa39b7e825
Richard Schneider <makaretu@gmail.com>
2017-02-13 16:53:52 +1300

    docs(INumberFormatter): describe the interface

--- src/Numbers/INumberFormatter.cs
+++ src/Numbers/INumberFormatter.cs
@@ -12,46 +12,102 @@ namespace Makaretu.Globalization.Numbers
     public interface INumberFormatter
     {
         /// <summary>
-        /// 
+        ///   Localises a <see cref="long"/>.
         /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
+        /// <param name="value">
+        ///  The number to localise.
+        /// </param>
+        /// <returns>
+        ///   The localised string representation of the <paramref name="value"/>
+        ///   according to the <see cref="Locale"/> and 
+        ///   <see cref="NumberOptions">formatting options</see>.
+        /// </returns>
         string ToString(long value);
 
         /// <summary>
-        /// 
+        ///   Localises a <see cref="decimal"/>.
         /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
+        /// <param name="value">
+        ///  The number to localise.
+        /// </param>
+        /// <returns>
+        ///   The localised string representation of the <paramref name="value"/>
+        ///   according to the <see cref="Locale"/> and 
+        ///   <see cref="NumberOptions">formatting options</see>.
+        /// </returns>
         string ToString(decimal value);
 
         /// <summary>
-        /// 
+        ///   Localises a <see cref="double"/>.
         /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
+        /// <param name="value">
+        ///  The number to localise.
+        /// </param>
+        /// <returns>
+        ///   The localised string representation of the <paramref name="value"/>
+        ///   according to the <see cref="Locale"/> and 
+        ///   <see cref="NumberOptions">formatting options</see>.
+        /// </returns>
         string ToString(double value);
 
         /// <summary>
-        /// 
+        ///   Localises a <see cref="long"/> currency.
         /// </summary>
-        /// <param name="value"></param>
-        /// <param name="currencyCode"></param>
-        /// <returns></returns>
+        /// <param name="value">
+        ///  The number to localise.
+        /// </param>
+        /// <param name="currencyCode">
+        ///    An <see href="https://en.wikipedia.org/wiki/ISO_4217">ISO 4217</see> currency code.  For example: "CNY", "NZD" or "JPY".
+        /// </param>
+        /// <returns>
+        ///   The localised string representation of the <paramref name="value"/> and
+        ///   <paramref name="currencyCode"/>
+        ///   according to the <see cref="Locale"/> and 
+        ///   <see cref="NumberOptions">formatting options</see>.
+        ///   <para>
+        ///   The number of significant digits is controlled by currency definition.
+        ///   </para>
+        /// </returns>
         string ToString(long value, string currencyCode);
 
         /// <summary>
+        ///   Localises a <see cref="double"/> currency.
         /// </summary>
-        /// <param name="value"></param>
-        /// <param name="currencyCode"></param>
-        /// <returns></returns>
+        /// <param name="value">
+        ///  The number to localise.
+        /// </param>
+        /// <param name="currencyCode">
+        ///    An <see href="https://en.wikipedia.org/wiki/ISO_4217">ISO 4217</see> currency code.  For example: "CNY", "NZD" or "JPY".
+        /// </param>
+        /// <returns>
+        ///   The localised string representation of the <paramref name="value"/> and
+        ///   <paramref name="currencyCode"/>
+        ///   according to the <see cref="Locale"/> and 
+        ///   <see cref="NumberOptions">formatting options</see>.
+        ///   <para>
+        ///   The number of significant digits is controlled by currency definition.
+        ///   </para>
+        /// </returns>
         string ToString(double value, string currencyCode);
 
         /// <summary>
+        ///   Localises a <see cref="decimal"/> currency.
         /// </summary>
-        /// <param name="value"></param>
-        /// <param name="currencyCode"></param>
-        /// <returns></returns>
+        /// <param name="value">
+        ///  The number to localise.
+        /// </param>
+        /// <param name="currencyCode">
+        ///    An <see href="https://en.wikipedia.org/wiki/ISO_4217">ISO 4217</see> currency code.  For example: "CNY", "NZD" or "JPY".
+        /// </param>
+        /// <returns>
+        ///   The localised string representation of the <paramref name="value"/> and
+        ///   <paramref name="currencyCode"/>
+        ///   according to the <see cref="Locale"/> and 
+        ///   <see cref="NumberOptions">formatting options</see>.
+        ///   <para>
+        ///   The number of significant digits is controlled by currency definition.
+        ///   </para>
+        /// </returns>
         string ToString(decimal value, string currencyCode);
     }
 }
