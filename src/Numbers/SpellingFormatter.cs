926a636e1d8c50add9ed90255695eb86c1f7ff75
Richard Schneider <makaretu@gmail.com>
2018-10-22 15:14:58 +1300

    feat(SpellingFormatter): spelling out numbers

---
+++ src/Numbers/SpellingFormatter.cs
@@ -0,0 +1,68 @@
+﻿using Sepia.Globalization.Numbers.Rules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sepia.Globalization.Numbers
+{
+    /// <summary>
+    ///   Localises an ordinal number into its textual representation.
+    /// </summary>
+    /// <remarks>
+    ///    In English the number 321 is represented as 'three hundred twenty-one'. 
+    /// </remarks>
+    public class SpellingFormatter
+    {
+        RulesetGroup spelloutRules;
+
+        /// <summary>
+        ///   The source for localisation information.
+        /// </summary>
+        public Locale Locale { get; set; }
+
+        /// <summary>
+        ///   Creates or reuses a spelling formatter for the specified <see cref="Locale"/>.
+        /// </summary>
+        /// <param name="locale">
+        ///   The locale.
+        /// </param>
+        /// <returns>
+        ///   The spelling formatter that is the best for the <paramref name="locale"/>.
+        /// </returns>
+        public static SpellingFormatter Create(Locale locale)
+        {
+            var formatter = new SpellingFormatter
+            {
+                Locale = locale,
+            };
+
+            var xml = locale
+                .ResourceBundle("common/rbnf/")
+                .FirstElement("ldml/rbnf/rulesetGrouping");
+            formatter.spelloutRules = RulesetGroup.Parse(xml);
+
+            return formatter;
+       }
+
+        /// <inheritdoc />
+        public string Format(long value)
+        {
+            return Format(Convert.ToDecimal(value));
+        }
+
+        /// <inheritdoc />
+        public string Format(decimal value)
+        {
+            return spelloutRules.Format(value, "spellout-numbering", Locale);
+        }
+
+        /// <inheritdoc />
+        public string Format(double value)
+        {
+            return spelloutRules.Format(value, "spellout-numbering", Locale);
+        }
+
+    }
+}
---
+++ test/Numbers/SpellingFormatterTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Collections.Generic;
+
+// http://demo.icu-project.org/icu4jweb/numero.jsp
+
+namespace Sepia.Globalization.Numbers
+{
+    [TestClass]
+    public class SpellingFormatterTest
+    {
+        [TestMethod]
+        public void Formatting_en_NZ_long()
+        {
+            var formatter = SpellingFormatter.Create(Locale.Create("en-NZ"));
+            Assert.AreEqual("minus one", formatter.Format(-1));
+            Assert.AreEqual("zero", formatter.Format(0));
+            Assert.AreEqual("thirteen", formatter.Format(13));
+            Assert.AreEqual("twenty", formatter.Format(20));
+            Assert.AreEqual("twenty-one", formatter.Format(21));
+            Assert.AreEqual("three hundred", formatter.Format(300));
+            Assert.AreEqual("three hundred twenty-one", formatter.Format(321));
+            Assert.AreEqual("one thousand two hundred thirty-four", formatter.Format(1234));
+            Assert.AreEqual("minus one thousand two hundred thirty-four", formatter.Format(-1234));
+            Assert.AreEqual("twenty quadrillion", formatter.Format(20000000000000000));
+            Assert.AreEqual("1,000,000,000,000,000,000", formatter.Format(1000000000000000000));
+
+            Assert.AreEqual("minus 9,223,372,036,854,775,808", formatter.Format(long.MinValue));
+            Assert.AreEqual("9,223,372,036,854,775,807", formatter.Format(long.MaxValue));
+        }
+
+        [TestMethod]
+        public void Formatting_en_NZ_decimal()
+        {
+            var formatter = SpellingFormatter.Create(Locale.Create("en-NZ"));
+            Assert.AreEqual("minus one", formatter.Format(-1m));
+            Assert.AreEqual("zero", formatter.Format(0m));
+            Assert.AreEqual("thirteen", formatter.Format(13m));
+            Assert.AreEqual("twenty", formatter.Format(20m));
+            Assert.AreEqual("twenty-one", formatter.Format(21m));
+            Assert.AreEqual("three hundred", formatter.Format(300m));
+            Assert.AreEqual("three hundred twenty-one", formatter.Format(321m));
+            Assert.AreEqual("one thousand two hundred thirty-four", formatter.Format(1234m));
+            Assert.AreEqual("minus one thousand two hundred thirty-four", formatter.Format(-1234m));
+            Assert.AreEqual("twenty quadrillion", formatter.Format(20000000000000000m));
+            Assert.AreEqual("1,000,000,000,000,000,000", formatter.Format(1000000000000000000m));
+
+            Assert.AreEqual("one point two", formatter.Format(1.2m));
+            Assert.AreEqual("one point zero two", formatter.Format(1.02m));
+            Assert.AreEqual("one point zero zero zero zero two", formatter.Format(1.00002m));
+            Assert.AreEqual("one point two", formatter.Format(1.2));
+            Assert.AreEqual("one point zero two", formatter.Format(1.02));
+            Assert.AreEqual("one point zero zero zero zero two", formatter.Format(1.00002));
+
+            Assert.AreEqual("minus 79,228,162,514,264,337,593,543,950,335", formatter.Format(decimal.MinValue));
+            Assert.AreEqual("79,228,162,514,264,337,593,543,950,335", formatter.Format(decimal.MaxValue));
+        }
+
+        [TestMethod]
+        public void Formatting_fr_FR_decimal()
+        {
+            var formatter = SpellingFormatter.Create(Locale.Create("fr-FR"));
+            Assert.AreEqual("moins un", formatter.Format(-1m));
+            Assert.AreEqual("zéro", formatter.Format(0m));
+            Assert.AreEqual("treize", formatter.Format(13m));
+            Assert.AreEqual("dix-neuf", formatter.Format(19m));
+            Assert.AreEqual("vingt", formatter.Format(20m));
+            Assert.AreEqual("vingt-et-un", formatter.Format(21m));
+            Assert.AreEqual("vingt-deux", formatter.Format(22m));
+            Assert.AreEqual("soixante-neuf", formatter.Format(69m));
+            Assert.AreEqual("soixante-dix-neuf", formatter.Format(79m));
+            Assert.AreEqual("quatre-vingt-dix-neuf", formatter.Format(99m));
+            Assert.AreEqual("trois cents", formatter.Format(300m));
+            Assert.AreEqual("trois cent vingt-et-un", formatter.Format(321m));
+            Assert.AreEqual("mille deux cent trente-quatre", formatter.Format(1234m));
+            Assert.AreEqual("moins mille deux cent trente-quatre", formatter.Format(-1234m));
+            Assert.AreEqual("vingt billiards", formatter.Format(20000000000000000m));
+
+            var sp = Cldr.Instance.CurrentVersion() < new Version(34, 0)
+                ? "\u00A0" : "\u202F";
+            Assert.AreEqual($"1{sp}000{sp}000{sp}000{sp}000{sp}000{sp}000", formatter.Format(1000000000000000000m));
+
+            Assert.AreEqual("un virgule deux", formatter.Format(1.2m));
+            Assert.AreEqual("un virgule zéro deux", formatter.Format(1.02m));
+            Assert.AreEqual("un virgule zéro zéro zéro zéro deux", formatter.Format(1.00002m));
+        }
+    }
+}
